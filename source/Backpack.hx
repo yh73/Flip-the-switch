4463e0d393644d737318c0632908079cf4105647
Haoyi <michael19971219@gmail.com>
2018-04-24 13:17:33 -0700

    Backpack upgraded

--- source/Backpack.hx
+++ source/Backpack.hx
@@ -5,26 +5,36 @@ import flixel.addons.display.FlxGridOverlay;
 import flixel.util.FlxColor;
 import openfl.events.Event;
 import flixel.FlxSprite;
-class Backpack extends FlxSprite
+import flixel.group.FlxGroup;
+class Backpack extends FlxTypedGroup<FlxSprite>
 {   
     var player:Character;
     var tileSize:Int;
+    var border:FlxSprite; 
+    var itemGroup:FlxTypedGroup<FlxSprite>;
     static var counter = 0;
 	override public function new(size:Int, number:Int, color:FlxColor, character:Character) 
 	{
-		super(0,0);
-        makeGraphic(5 * size, size, color);
-        FlxGridOverlay.overlay(this, size, size,  number * size, 
-            size, true, color);
+		super();
+        border = new FlxSprite(0,0).makeGraphic(5 * size, size, color);
+        this.add(FlxGridOverlay.overlay(border, size, size,  number * size, 
+            size, true, color));
+        
+        this.add(new FlxSprite(border.x, border.y).loadGraphic("assets/health.png", 32, 32));
         player = character;
         tileSize = size;
         visible = false;
 	}
 	
+
+
     override public function update(elapsed:Float):Void 
-    {
-        x = player.x - 4.5 * tileSize / 2;
-        y = player.y + tileSize;
+    {   
+        for (item in this) {
+            item.x = player.x - 4.5 * tileSize / 2;
+            item.y = player.y + tileSize;
+        }
+       
         if (FlxG.keys.justPressed.B) {
             visible = !visible;
         }
