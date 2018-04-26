49472678c7c4b1265e3d418f620981a060abc6ab
Haoyi <michael19971219@gmail.com>
2018-04-26 11:30:17 -0700

    s

--- source/Backpack.hx
+++ source/Backpack.hx
@@ -12,11 +12,14 @@ import flixel.addons.ui.FlxButtonPlus;
 class Backpack extends FlxTypedGroup<FlxSprite>
 {   
     public var border:FlxSprite;
-    public var dropDown:FlxUIDropDownMenu;
     public var buttons:FlxTypedGroup<FlxButtonPlus>;
+    public var equipSlotBorder:FlxSprite;
     public var equipSlot:FlxSprite;
+    public var firstTimeEquip = true;
     var player:Character;
     var tileSize:Int; 
+    var lastItemIdx:Int;
+    var button:FlxButtonPlus;
     static var counter = 0;
 
 	override public function new(size:Int, number:Int, color:FlxColor, character:Character) 
@@ -25,20 +28,21 @@ class Backpack extends FlxTypedGroup<FlxSprite>
         player = character;
         tileSize = size;
         buttons = new FlxTypedGroup<FlxButtonPlus>();
-        equipSlot = new FlxSprite(0,0).makeGraphic(size, size, color);
+        equipSlotBorder = new FlxSprite(0,0).makeGraphic(size, size, color);
+        equipSlot = new FlxSprite(0,0,null);
         border = new FlxSprite(0,0).makeGraphic(5 * size, size, color);
         border = FlxGridOverlay.overlay(border, size, size,  number * size, 
             size, true, color);
-        buttons.add(new FlxButtonPlus(0,0, null,"Equip", 32, 16));
+        button = new FlxButtonPlus(0,0, equip,"Equip", 32, 16);
+        buttons.add(button);
         buttons.add(new FlxButtonPlus(0,0, null,"Craft", 32, 16));
-        dropDown = new FlxUIDropDownMenu(0, 0,
-            FlxUIDropDownMenu.makeStrIdLabelArray(["Equip", "Craft"]),
-            new FlxUIDropDownHeader(64));
-        this.add(new FlxSprite(border.x, border.y).loadGraphic("assets/health.png", 32, 32));
+        this.add(new FlxSprite(border.x, border.y).loadGraphic("assets/gold.png", 32, 32));
+        this.add(new FlxSprite(border.x, border.y).loadGraphic("assets/gold.png", 32, 32));
         this.add(new FlxSprite(border.x, border.y).loadGraphic("assets/health.png", 32, 32));
         visible = false;
         buttons.kill();
         border.visible = false;
+        equipSlotBorder.visible = false;
         equipSlot.visible = false;
 	}
 	
@@ -49,17 +53,16 @@ class Backpack extends FlxTypedGroup<FlxSprite>
         var i = 0;
         border.x = player.x - 4.5 * tileSize / 2;
         border.y = player.y + tileSize;
-        equipSlot.x = player.x - tileSize;
-        equipSlot.y = player.y - tileSize / 2;
+        if (equipSlot != null) {
+            equipSlot.x = player.x - tileSize;
+            equipSlot.y = player.y - tileSize / 2;
+        }
+        equipSlotBorder.x = player.x - tileSize;
+        equipSlotBorder.y = player.y - tileSize / 2;
         for (item in this) {
             item.x = player.x - 4.5 * tileSize / 2 + 32 * i;
             item.y = player.y + tileSize;
             if (FlxG.mouse.overlaps(item, null) && border.visible) {
-                /*
-                dropDown.revive();
-                dropDown.x = item.x;
-                dropDown.y = item.y;
-                */
                 var j = 0;
                 buttons.revive();
                 for (button in buttons) 
@@ -68,22 +71,42 @@ class Backpack extends FlxTypedGroup<FlxSprite>
                     button.y = item.y + 16 * j;
                     j++;
                 }
-            } else if (FlxG.mouse.justPressed) {
+            } else if (!FlxG.mouse.overlaps(border, null)) {
                 buttons.kill();
             }
             i++;
+            
         }
        
         if (FlxG.keys.justPressed.B) {
             buttons.kill();
             visible = !visible;
-            equipSlot.visible = !equipSlot.visible;
+            if (!firstTimeEquip) {
+                equipSlot.visible = !equipSlot.visible;
+            }
+            equipSlotBorder.visible = !equipSlotBorder.visible;
             border.visible = !border.visible;
         }
     }
 
     private function equip() 
-    {
+    {   
+        var itemToEquip = new FlxSprite(0,0);
+        for (item in this) {
+            if (item.x == button.x) {
+                itemToEquip = item;
+                break;
+            }
+        }
+        lastItemIdx = this.members.indexOf(itemToEquip);
+        if (!firstTimeEquip) {
+            this.insert(lastItemIdx, new FlxSprite(0,0).loadGraphicFromSprite(equipSlot));
+            equipSlot.loadGraphicFromSprite(this.remove(itemToEquip));
+        } else {
+            equipSlot.loadGraphicFromSprite(this.remove(itemToEquip));
+            equipSlot.visible = true;
+            firstTimeEquip = false;
+        }
         buttons.kill();
     }
 }
