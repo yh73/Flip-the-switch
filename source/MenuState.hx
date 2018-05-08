3014a770bdcebbf45e4b300e73c80fa6ae668a75
Haoyi <michael19971219@gmail.com>
2018-05-08 16:33:15 -0700

    menu

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -19,37 +19,32 @@ class MenuState extends FlxState
 	private var intro:FlxText;
 	private var switchOn:FlxSprite;
 	private var switchOff:FlxSprite;
+    private var levels:FlxTypedGroup<FlxButton>;
 	private static var youDied:Bool = false;
 	
 	override public function create():Void 
 	{   
-        intro = new FlxText(180,80, "Flip the Switch", 30);
+        levels = new FlxTypedGroup<FlxButton>();
+        intro = new FlxText(180,60, "Flip the Switch", 30);
         //add(_btnPlay);
-		switchOff = new FlxSprite(195, 130).loadGraphic("assets/switchOff.png");
-		switchOn = new FlxSprite(195, 81).loadGraphic("assets/switchOn.png");
-		intro.alignment = FlxTextAlign.CENTER;
-		add(switchOff);
-		add(switchOn);
+        var i: Int;
+        for (i in 0...25) {
+            var level = new FlxButton(120 + i % 5 * 80, 120 + Std.int(i / 5) * 60, Std.string(i + 1), loadLevel);
+            levels.add(level);
+        }
 		add(intro);
-		switchOn.kill();
+        add(levels);
 		super.create();
 		
 	}
 
-	private function onTimer(Timer:FlxTimer):Void {
-		FlxG.switchState(new PlayState(0));
+	private function loadLevel():Void {
+        var levelNumber = Std.int((FlxG.mouse.x - 120) / 80) + 5 * Std.int((FlxG.mouse.y - 120) / 60);
+		FlxG.switchState(new PlayState(levelNumber));
 	}
 
 	override public function update(elapsed:Float):Void 
 	{	
-		if (FlxG.mouse.overlaps(switchOff, null) && FlxG.mouse.justPressed) {
-			switchOff.kill();
-			switchOn.revive();
-			var timer = new FlxTimer();
-			Main.LOGGER.logLevelStart(0);
-			timer.start(0.05, onTimer, 1);
-		}
-		
 		super.update(elapsed);
 	}
 	
