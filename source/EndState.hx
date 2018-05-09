91ce42429fa7e140c40595e9598f061c536a270e
Haoyi <michael19971219@gmail.com>
2018-05-08 18:06:47 -0700

    s

--- source/EndState.hx
+++ source/EndState.hx
@@ -23,20 +23,33 @@ class EndState extends FlxState
 	
 	override public function create():Void 
 	{   
-        intro = new FlxText(130,80, "More Coming Soon", 30);
-		var button = new FlxButton(260, 140, "Restart", menu);
+        intro = new FlxText(160,80, "More coming soon", 30);
+        //add(_btnPlay);
+		switchOff = new FlxSprite(195, 130).loadGraphic("assets/switchOff.png");
+		switchOn = new FlxSprite(195, 81).loadGraphic("assets/switchOn.png");
+		intro.alignment = FlxTextAlign.CENTER;
+		add(switchOff);
+		add(switchOn);
 		add(intro);
-		add(button);
+		switchOff.kill();
 		super.create();
 		
 	}
 
-	private function menu():Void {
+	private function onTimer(Timer:FlxTimer):Void {
 		FlxG.switchState(new StartState());
 	}
 
 	override public function update(elapsed:Float):Void 
 	{	
+		if (FlxG.mouse.overlaps(switchOff, null) && FlxG.mouse.justPressed) {
+			switchOn.kill();
+			switchOff.revive();
+			var timer = new FlxTimer();
+			Main.LOGGER.logLevelStart(0);
+			timer.start(0.1, onTimer, 1);
+		}
+		
 		super.update(elapsed);
 	}
 	
